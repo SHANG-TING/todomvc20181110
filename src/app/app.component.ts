e1116b0c3619f0a5b60178e564b82841f027fa3e
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-10 14:06:05 +0800

    綁定todo的基本功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  name: string;
+  checked: boolean;
+  completed: boolean;
+  isEdit: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +16,39 @@ export class AppComponent {
   appTitle = 'Todos';
   placeholderText = 'What needs to be done?? O_O';
   background = 'green';
+  todos: Todo[] = [];
+  get totalCount() {
+    return this.todos.filter(x => x.checked).length;
+  }
   todoValue = '';
 
   newTodo(inputElement) {
-    console.log(inputElement.value);
+    const todo: Todo = {
+      name: inputElement.value,
+      checked: false,
+      completed: false,
+      isEdit: false,
+    };
+    this.todos.push(todo);
+  }
+
+  removeTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+  selectAll() {
+    this.todos.map(x => (x.checked = true));
+  }
+
+  clearComplete() {
+    this.todos.map(x => (x.completed = false));
+  }
+
+  onActive() {
+    this.todos.filter(x => x.checked).map(x => (x.completed = false));
+  }
+
+  onComplete() {
+    this.todos.filter(x => x.checked).map(x => (x.completed = true));
   }
 }
