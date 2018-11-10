8ae772b60351acacf1fca466ea3aa9c3bd8a3b88
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-10 21:33:41 +0800

    測試一些語法 (非課程內容)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,25 @@ export class AppComponent {
   placeholderText = 'What needs to be done?? O_O';
   background = 'green';
   get todos() {
-    switch (this.selectedStatus) {
-      case 'Active':
-        return this.dataService.todos.filter(x => !x.isCompleted);
-      case 'Completed':
-        return this.dataService.todos.filter(x => x.isCompleted);
-      default:
-        return this.dataService.todos;
-    }
+    // switch (this.selectedStatus) {
+    //   case 'Active':
+    //     return this.dataService.todos.filter(x => !x.isCompleted);
+    //   case 'Completed':
+    //     return this.dataService.todos.filter(x => x.isCompleted);
+    //   default:
+    //     return this.dataService.todos;
+    // }
+    return this.dataService.todos.filter(this.displayStatusFuncs[this.selectedStatus]);
   }
   get itemLeft() {
     return this.dataService.todos.filter(x => !x.isCompleted).length;
   }
   displayStatus = ['All', 'Active', 'Completed'];
+  displayStatusFuncs = {
+    Completed: (x: any) => !x.isCompleted,
+    Active: (x: any) => x.isCompleted,
+    All: (x: any) => true,
+  };
   selectedStatus = 'All';
   todoValue = '';
 
--- src/app/shared/models/todo.model.ts
+++ src/app/shared/models/todo.model.ts
@@ -1,4 +1,5 @@
 export interface Todo {
   name: string;
   isCompleted: boolean;
+  isEdit?: boolean;
 }
